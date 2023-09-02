c5eb4ef45fbde5ed1e17f4f4503d3ea813ef0766
VladF <Vlad_Fisak@mail.ru>
2023-09-02 23:28:30 +0300

    Add workflow with PEP8 linting v3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,19 @@
 """Файл конфигурации проекта Taski-docker."""
-import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+HOSTS = os.environ['ALLOWED_HOSTS']
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.31.198', '127.0.0.1', 'localhost', 'taskyp.ddns.net']
+ALLOWED_HOSTS = HOSTS.split()
 
 # Application definition
 
