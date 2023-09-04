3448c49d5803cf90f106422f39c048f5979fa9db
VladF <Vlad_Fisak@mail.ru>
2023-09-04 16:25:38 +0300

    Add workflow with PEP8 linting v16

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,13 +2,9 @@
 import os
 from pathlib import Path
 
-from dotenv import find_dotenv, load_dotenv
-
-load_dotenv(find_dotenv())
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = False
 
